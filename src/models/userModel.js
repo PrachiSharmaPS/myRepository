10fd0fb6123f1ff573d4293f017bd0e4478ff75f
PrachiSharmaPS <prachisharma.ps01@gmail.com>
2022-10-23 21:48:56 +0530

    book collection in database

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,18 +1,10 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema( {
-    firstName: String,
-    lastName: String,
-    mobile: {
-        type: String,
-        unique: true,
-        required: true
-    },
-    emailId: String,
-    gender: {
-        type: String,
-        enum: ["male", "female", "LGBTQ"] //"falana" will give an error
-    },
+    bookName: String,
+    authorName: String,
+    year: String,
+    category: String,
     age: Number,
     // isIndian: Boolean,
     // parentsInfo: {
@@ -23,7 +15,7 @@ const userSchema = new mongoose.Schema( {
     // cars: [ String  ]
 }, { timestamps: true });
 
-module.exports = mongoose.model('User', userSchema) //users
+module.exports = mongoose.model('User1', userSchema) //users
 
 
 
